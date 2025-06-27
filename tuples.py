b43ccb51e4a43c1e2dee587a2ec3851939cd1b1b
BraNtswembuu <obakeng.boikanyo270@gmail.com>
2025-06-27 21:30:19 +0200

    Add list and tuple operations with documentation

---
+++ tuples.py
@@ -0,0 +1,15 @@
+# Tuples
+
+my_tuple= [1, 2, 3, 4, 5]
+print(my_tuple)  # Accessing the first element
+print(my_tuple[0])  # Accessing the first element
+print(my_tuple[2])
+print(my_tuple[-1])  # Accessing the last element
+
+tuples1 = (1, 2, 3, 4, 5)  # Creating a tuple
+tuples2 = (6, 7, 8, 9, 10)  # Another tuple
+
+conc_tuple = tuples1 + tuples2  # Concatenating tuples
+print(conc_tuple)  # Output: (1, 2, 3,
+rep_tuple = tuples1 * 2  # Repeating a tuple
+print(rep_tuple)  # Output: (1, 2, 3, 4
