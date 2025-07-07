7a832124fefd176dd23ce093a002a3f6586f0f57
BraNtswembuu <obakeng.boikanyo270@gmail.com>
2025-07-07 15:28:19 +0200

    Add modules.py

---
+++ modules.py
@@ -0,0 +1,9 @@
+# Import the module using relative import
+import math_operations as math_ops
+
+# Use the functions from the imported module
+print(math_ops.add(5, 3))  # Output: 8
+print(math_ops.subtract(10, 4))  # Output: 6
+
+print(math_ops.multiply(2, 6))  # Output: 12
+print(math_ops.divide(8, 2))  # Output: 4.0
